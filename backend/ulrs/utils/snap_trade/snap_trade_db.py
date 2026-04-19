fdd4667feb06898a44e16b2f025ad1b9bb049e1f
codewani <kondwanimwape013@gmail.com>
2026-04-18 22:12:35 -0500

    rem unnecessary line from snap_trade_db

--- backend/ulrs/utils/snap_trade/snap_trade_db.py
+++ backend/ulrs/utils/snap_trade/snap_trade_db.py
@@ -4,9 +4,6 @@ from sqlalchemy.orm import Session
 from backend.models.auth.user import UserSecret
 
 
-router = APIRouter(prefix="/snap_trade", tags=["snap_trade"])
-
-
 def get_snap_trade_secret(db: Session, user_id: str) -> str:
     secret_row = db.query(UserSecret).filter(UserSecret.user_id == user_id).first()
     if not secret_row:
